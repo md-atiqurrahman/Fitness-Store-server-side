2e547f1d2a6d136f6312b9fdcce54112b75a2756
md-atiqurrahman <mdatiqurrahman9104@gmail.com>
2022-05-14 16:51:01 +0600

    initialize server

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config()
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/', (req, res) =>{
+    res.send('Fitness Store server is running')
+});
+
+app.listen(port, () =>{
+    console.log('Listening on port',port)
+})
