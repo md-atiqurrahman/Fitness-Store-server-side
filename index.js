2c2652aa83bf927f98f9d6abf2a4ce03bd346d3f
md-atiqurrahman <mdatiqurrahman9104@gmail.com>
2022-05-15 17:10:35 +0600

    get the data from database by get operation

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
 require('dotenv').config()
 const app = express();
@@ -27,6 +27,13 @@ async function run(){
            const cursor = productCollection.find(query);
            const products = await cursor.toArray();
            res.send(products);
+       });
+
+       app.get('/products/:id', async (req, res) =>{
+           const id = req.params.id;
+           const query = {_id: ObjectId(id)};
+           const product = await productCollection.findOne(query);
+           res.send(product);
        })
 
 
