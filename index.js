584e870ed65e24add128658d776efa728605aa66
md-atiqurrahman <mdatiqurrahman9104@gmail.com>
2023-09-07 21:31:01 +0600

    add vercel.json file

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const cors = require('cors');
-const jwt = require('jsonwebtoken');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const { ObjectID } = require('bson');
-require('dotenv').config()
+const express = require("express");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const { ObjectID } = require("bson");
+require("dotenv").config();
 const app = express();
 
 const port = process.env.PORT || 5000;
@@ -12,119 +12,120 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
 function verifyJWT(req, res, next) {
-    const authHeader = req.headers.authorization;
-    if (!authHeader) {
-        return res.status(401).send({ message: 'unAuthorized access' })
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: "unAuthorized access" });
+  }
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(403).send({ message: "Forbidden access" });
     }
-    const token = authHeader.split(' ')[1];
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-        if (err) {
-            return res.status(403).send({ message: 'Forbidden access' })
-        }
-        req.decoded = decoded;
-        next();
-    })
+    req.decoded = decoded;
+    next();
+  });
 }
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster2.85unk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
 async function run() {
-    try {
-        await client.connect();
+  try {
+    client.connect(); // await client.connect();
 
-        const productCollection = client.db('gymEquipment').collection('product');
+    const productCollection = client.db("gymEquipment").collection("product");
 
+    //Auth
+    app.post("/getToken", (req, res) => {
+      const user = req.body;
+      const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1d",
+      });
+      res.send({ accessToken });
+    });
 
-        //Auth
-        app.post('/getToken', (req, res) => {
-            const user = req.body;
-            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-                expiresIn: '1d'
-            })
-            res.send({ accessToken })
-        })
+    //Server api
+    app.get("/products", async (req, res) => {
+      const query = {};
+      const cursor = productCollection.find(query);
+      const products = await cursor.toArray();
+      res.send(products);
+    });
 
+    app.get("/addedProduct", verifyJWT, async (req, res) => {
+      const decodedEmail = req.decoded.email;
+      const email = req.query.email;
+      if (email === decodedEmail) {
+        const query = { email: email };
+        const cursor = productCollection.find(query);
+        const myProducts = await cursor.toArray();
+        res.send(myProducts);
+      } else {
+        res.status(403).send({ message: "Forbidden access" });
+      }
+    });
 
-        //Server api
-        app.get('/products', async (req, res) => {
-            const query = {};
-            const cursor = productCollection.find(query);
-            const products = await cursor.toArray();
-            res.send(products);
-        });
+    app.delete("/addedProduct/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await productCollection.deleteOne(query);
+      res.send(result);
+    });
 
-        app.get('/addedProduct', verifyJWT, async (req, res) => {
-            const decodedEmail = req.decoded.email;
-            const email = req.query.email;
-            if (email === decodedEmail) {
-                const query = { email: email };
-                const cursor = productCollection.find(query);
-                const myProducts = await cursor.toArray();
-                res.send(myProducts);
-            }
-            else{
-                res.status(403).send({message: 'Forbidden access'})
-            }
-        });
+    app.post("/products", async (req, res) => {
+      const product = req.body;
+      const result = await productCollection.insertOne(product);
+      res.send(result);
+    });
 
-        app.delete('/addedProduct/:id', async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: ObjectId(id) };
-            const result = await productCollection.deleteOne(query);
-            res.send(result);
-        })
+    app.get("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const product = await productCollection.findOne(query);
+      res.send(product);
+    });
 
-        app.post('/products', async (req, res) => {
-            const product = req.body;
-            const result = await productCollection.insertOne(product);
-            res.send(result)
-        })
+    app.put("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const newQuantity = req.body.quantity;
+      const filter = { _id: ObjectID(id) };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          quantity: newQuantity,
+        },
+      };
+      const result = await productCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      res.send(result);
+    });
 
-        app.get('/products/:id', async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: ObjectId(id) };
-            const product = await productCollection.findOne(query);
-            res.send(product);
-        });
-
-        app.put('/products/:id', async (req, res) => {
-            const id = req.params.id;
-            const newQuantity = req.body.quantity;
-            const filter = { _id: ObjectID(id) }
-            const options = { upsert: true };
-            const updateDoc = {
-                $set: {
-                    quantity: newQuantity
-                },
-            };
-            const result = await productCollection.updateOne(filter, updateDoc, options)
-            res.send(result);
-        });
-
-        app.delete('/products/:id', async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: ObjectId(id) };
-            const result = await productCollection.deleteOne(query);
-            res.send(result);
-        })
-
-    }
-    finally {
-        // await client.close();
-    }
+    app.delete("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await productCollection.deleteOne(query);
+      res.send(result);
+    });
+  } finally {
+    // await client.close();
+  }
 }
 
 run().catch(console.dir);
 
-app.get('/', (req, res) => {
-    res.send('Fitness Store server is running')
+app.get("/", (req, res) => {
+  res.send("Fitness Store server is running");
 });
 
 app.listen(port, () => {
-    console.log('Listening on port', port)
-})
+  console.log("Listening on port", port);
+});
